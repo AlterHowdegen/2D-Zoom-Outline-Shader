3b13bb788e172da566d434d5a663010056674f0e
Johannes <johannes.nichell@googlemail.com>
2026-02-01 00:11:31 +0100

    More animations

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -6,6 +6,9 @@ public class Rotator : MonoBehaviour
 {
     public List<GameObject> gameObjects = new List<GameObject>();
     public float rotationDuration;
+    public float scaleSmall;
+    public float scaleBig;
+
     void Start()
     {
 
@@ -24,6 +27,32 @@ public class Rotator : MonoBehaviour
     public void BarrelRoll(GameObject go)
     {
         LeanTween.rotateAroundLocal(go, Vector3.right, 360f, rotationDuration)
+            .setEaseInOutCubic();
+    }
+
+    [ContextMenu("Resize Small")]
+    public void ResizeAllSmall()
+    {
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            GameObject go = gameObjects[i];
+            Resize(go, scaleSmall);
+        }
+    }
+
+    [ContextMenu("Resize Big")]
+    public void ResizeAllBig()
+    {
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            GameObject go = gameObjects[i];
+            Resize(go, scaleBig);
+        }
+    }
+
+    public void Resize(GameObject go, float scale)
+    {
+        LeanTween.scale(go, Vector3.one * scale, rotationDuration)
             .setEaseInOutCubic();
     }
 }
