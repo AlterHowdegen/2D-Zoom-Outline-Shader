61403c61a3426822b021389e663ad68ff4e7d665
Johannes <johannes.nichell@googlemail.com>
2026-02-01 00:49:11 +0100

    Moar animations

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
@@ -8,15 +9,30 @@ public class Rotator : MonoBehaviour
     public float rotationDuration;
     public float scaleSmall;
     public float scaleBig;
+    public Vector3 defaultRotation;
+    public Vector3 turntableStartRotation;
+    private bool turntableRunning;
+    private bool resizedBig;
+    private bool isBarrelRolling;
 
     void Start()
     {
-
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            GameObject go = gameObjects[i];
+            go.transform.localScale = Vector3.one * scaleSmall;
+        }
     }
 
     [ContextMenu("Barrel Roll")]
     public void BarrelRollAll()
     {
+        if (isBarrelRolling)
+        {
+            return;
+        }
+
+        isBarrelRolling = true;
         for (int i = 0; i < gameObjects.Count; i++)
         {
             GameObject go = gameObjects[i];
@@ -27,24 +43,27 @@ public class Rotator : MonoBehaviour
     public void BarrelRoll(GameObject go)
     {
         LeanTween.rotateAroundLocal(go, Vector3.right, 360f, rotationDuration)
-            .setEaseInOutCubic();
+            .setEaseInOutCubic()
+            .setOnComplete(() => isBarrelRolling = false);
     }
 
-    [ContextMenu("Resize Small")]
-    public void ResizeAllSmall()
+    [ContextMenu("Resize")]
+    public void ResizeAll()
     {
-        for (int i = 0; i < gameObjects.Count; i++)
+        if (resizedBig)
         {
-            GameObject go = gameObjects[i];
-            Resize(go, scaleSmall);
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                resizedBig = false;
+                GameObject go = gameObjects[i];
+                Resize(go, scaleSmall);
+            }
+            return;
         }
-    }
 
-    [ContextMenu("Resize Big")]
-    public void ResizeAllBig()
-    {
         for (int i = 0; i < gameObjects.Count; i++)
         {
+            resizedBig = true;
             GameObject go = gameObjects[i];
             Resize(go, scaleBig);
         }
@@ -54,5 +73,53 @@ public class Rotator : MonoBehaviour
     {
         LeanTween.scale(go, Vector3.one * scale, rotationDuration)
             .setEaseInOutCubic();
+    }
+
+    [ContextMenu("Turntable")]
+    public void TurntableAll()
+    {
+        LeanTween.cancelAll();
+        if (turntableRunning)
+        {
+            turntableRunning = false;
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                GameObject go = gameObjects[i];
+                DefaultPosition(go);
+            }
+            return;
+        }
+
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            turntableRunning = true;
+            GameObject go = gameObjects[i];
+            TurntablePre(go);
+        }
+    }
+
+    private void DefaultPosition(GameObject go)
+    {
+        LeanTween.rotateLocal(go, defaultRotation, rotationDuration / 2f)
+            .setEaseOutCubic();
+    }
+
+    public void TurntablePre(GameObject go)
+    {
+        LeanTween.rotateLocal(go, turntableStartRotation, rotationDuration / 2f)
+            .setEaseInCubic()
+            .setOnComplete(() => TurntableStart(go));
+    }
+
+    private void TurntableStart(GameObject go)
+    {
+        LeanTween.rotateAround(go, Vector3.up, 360f, rotationDuration * 2f)
+            .setOnComplete(() => TurntableLoop(go));
+    }
+
+    private void TurntableLoop(GameObject go)
+    {
+        LeanTween.rotateAround(go, Vector3.up, 360f, rotationDuration * 2f)
+            .setLoopCount(0);
     }
 }
